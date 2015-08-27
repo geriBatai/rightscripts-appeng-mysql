af68c26ee121ea9f6792da71af5ba12d898d0d06
Simas Cepaitis <simas.cepaitis@snc.lt>
2015-08-27 13:46:02 +0100

    Execute setup.sh via /bin/sh

--- collectd.sh
+++ collectd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 path=$(dirname $0)
-${path}/setup.sh
+/bin/sh ${path}/setup.sh
 
 /opt/chef/bin/chef-solo -c /tmp/appeng-config.rb -j /tmp/appeng-inputs.json -o 'recipe[rs-mysql::collectd]'
