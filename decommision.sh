fe42dd8aae685abeed8c0bfe7c019568689de4af
Simas Cepaitis <simas.cepaitis@snc.lt>
2015-08-28 12:32:39 +0100

    Execute chef-solo via sudo

--- decommision.sh
+++ decommision.sh
@@ -3,4 +3,4 @@
 path=$(dirname $0)
 /bin/sh ${path}/setup.sh
 
-/opt/chef/bin/chef-solo -c /tmp/appeng-config.rb -j /tmp/appeng-inputs.json -o 'recipe[rs-mysql::decommision]'
+sudo /opt/chef/bin/chef-solo -c /tmp/appeng-config.rb -j /tmp/appeng-inputs.json -o 'recipe[rs-mysql::decommision]'
