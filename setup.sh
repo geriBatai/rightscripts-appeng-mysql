8f703ca9721845863bf5ce753efee228b5740a95
Simas Cepaitis <simas.cepaitis@snc.lt>
2015-09-01 16:27:06 +0100

    Fix path

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ for d in $(ls -d /var/spool/rightlink/cookbooks/*/) ; do
 	fi
 done
 
-/opt/chef/bin/ruby <<EOF > /tmp/appeng-inputs.json
+/opt/chef/embedded/bin/ruby <<EOF > /tmp/appeng-inputs.json
 require 'rubygems'
 require 'json'
 
